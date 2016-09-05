38bbdf02f35b0ce844385d04817b3350315f10a9
Yijunmaverick <liyjcse@gmail.com>
2016-09-05 10:37:17 -0700

    Update demo_train.m

--- examples/Train/demo_train.m
+++ examples/Train/demo_train.m
@@ -1,6 +1,6 @@
 close all;clc;
 if ~exist('imdb')
-    imdb = load('.\TrainingData\loaddb_depth_nyu_1000.mat') ;
+    imdb = load('./TrainingData/loaddb_depth_nyu_1000.mat') ;
 
     imdb.images.data = single(imdb.images.data(:,:,:,1:192000));
     imdb.images.labels = single(imdb.images.labels(:,:,:,1:192000));
