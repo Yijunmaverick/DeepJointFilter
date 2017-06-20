d0265e1ab708921753ebd27921c72c993120f9b6
Yijun Li <liyjcse@gmail.com>
2017-06-20 10:08:04 -0700

    Update cnn_test_noise_reduction.m

--- examples/Test/cnn_test_noise_reduction.m
+++ examples/Test/cnn_test_noise_reduction.m
@@ -5,19 +5,19 @@ close all;
 flash = 'carpet_00_flash.jpg';
 nonflash = 'carpet_01_noflash.jpg';
 
-%% target image
+% target image
 image  = imread(nonflash);
 [h,w,s]=size(image);
 image = double(image)./255;
   
-%% guided image
+% guided image
 Guide_image = imread(flash);
 Guide_image = double(Guide_image)./255;
     
 input = single(zeros(h,w,4));
 input(:,:,2:4) = Guide_image;
     
-%% load model    
+% load model
 load('NoiseReduction.mat')
 net1.layers(1,1:10)=net.layers(1,1:10);
     
