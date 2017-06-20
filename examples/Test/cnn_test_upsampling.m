15e587c652cac40a688066a9af8ae30841bc4a81
Yijun Li <liyjcse@gmail.com>
2017-06-20 10:08:48 -0700

    Update cnn_test_upsampling.m

--- examples/Test/cnn_test_upsampling.m
+++ examples/Test/cnn_test_upsampling.m
@@ -2,28 +2,28 @@ close all;
 
 up_scale = 8;
 
-%% target image
+% target image
 im = imread('1025_big.png');
 im = double(im)./255;
 im = modcrop(im, up_scale);
 
-%% guided image
+% guided image
 Guide_image = imread('1025_color.png');  
 Guide_image = double(Guide_image)./255;
 Guide_image = modcrop(Guide_image, up_scale);
 [h,w,~]=size(Guide_image);
  
-%% generate low-res 
+% generate low-res
 im_l = im(up_scale:up_scale:end, up_scale:up_scale:end);
 
-%% first apply bicubic upsampling
+% first apply bicubic upsampling
 im_l = imresize(im_l, [h w], 'bicubic');
 
 input = single(zeros(h,w,4));
 input(:,:,1) = im_l;
 input(:,:,2:4) = Guide_image;
 
-%% filtering
+% filtering
 load('Upsampling_8x.mat')
 net1.layers(1,1:10)=net.layers(1,1:10);
 res=vl_simplenn(net1,input);
